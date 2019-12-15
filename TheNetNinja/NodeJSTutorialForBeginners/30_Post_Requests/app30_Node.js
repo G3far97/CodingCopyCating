42d952e2984f37f7131fb7f57ec193d617f01fda
G3far97 <ahmed.fouadgaafar@student.guc.edu.eg>
2019-12-15 11:08:10 +0200

    Updated: 30_Post_Requests

--- TheNetNinja/NodeJSTutorialForBeginners/30_Post_Requests/app30_Node.js
+++ TheNetNinja/NodeJSTutorialForBeginners/30_Post_Requests/app30_Node.js
@@ -1,6 +1,9 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 const app = express();
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.set('view engine', 'ejs');
 app.use('/assets', express.static('assets'));
@@ -14,9 +17,9 @@ app.get('/contact', function(req, res) {
     res.render('contact30_Node', {qs: req.query});
 });
 
-app.post('/contact', function(req, res) {
+app.post('/contact', urlencodedParser, function(req, res) {
     console.log(req.body);
-    res.render('contact30_Node', {qs: req.query});
+    res.render('contact-success30_Node', {data: req.body});
 });
 
 app.get('/profile/:name', function(req, res) {
