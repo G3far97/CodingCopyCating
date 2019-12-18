776e9ced27218fe9b386404e7634e8cb507863e2
G3far97 <ahmed.fouadgaafar@student.guc.edu.eg>
2019-12-19 01:47:22 +0200

    Updated: 04_ArrowFunction_SyntaxAndExamples

--- ElzeroWebSchool/Tutorials/LearnECMAScript6InArabic/04_ArrowFunction_SyntaxAndExamples/main04_ES6.js
+++ ElzeroWebSchool/Tutorials/LearnECMAScript6InArabic/04_ArrowFunction_SyntaxAndExamples/main04_ES6.js
@@ -1,62 +1,79 @@
-/*global console, window*/
-
 /*
-    New Way To Create Variables let, const
+    Arrow Function:
+    Identifier => Expression
 
-    var
-    - Function Scope
-    - Can Be ReDeclared
-    - Undefined When Accessing A Variable Before It's Declared
-    - Create Properties in the Window Object
 
-    let
-    - Block Scope
-    - Can Not Be ReDeclared
-    - ReferenceError When Accessing A Variable Before It's Declared
-    - Does Not Create Properties in the Window Object
-    = Can Be Updated Later
+    remove the "function" keyword and add an arrow operator before the function body
+    one statement to be executed?: You can remove the curly brackets and the "return".
+    one parameter?: You can remove the brackets of parameters.
 
-    const
-    - Block Scope
-    - Can Not Be ReDeclared
-    - ReferenceError When Accessing A Variable Before It's Declared
-    - Does Not Create Properties in the Window Object
-    = Can Not Be Updated Later => TypeError
-
+    IMPORTANT NOTE: Arrow function DOES NOT replace the regular function.
 */
 
 /////////////////////////
 
-// const SETTINGS = 1;
-// var SETTINGS = 2;
-// console.log(SETTINGS); // SyntaxError: Identifier 'SETTINGS' has already been declared
-
-const SETTINGS = 1;
-if(true) {
-    const SETTINGS = 2;
-    console.log(SETTINGS); // 2
+function test() {
+    return 2;
 }
-console.log(SETTINGS); // 1
 
-const LANGUAGES = [];
-LANGUAGES.push("HTML", "CSS", "JS");
-console.log(LANGUAGES); // (3) ["HTML", "CSS", "JS"]
-
-const OBJECT = {
-    name: "Osama",
-    sex: "male"
-}
-OBJECT.name = "Ahmed";
-console.log(OBJECT); // {name: "Ahmed", sex: "male"}
-
-const OBJECT2 = {
-    name: "Ahmed",
-    sex: "male"
-}
-Object.freeze(OBJECT2);
-OBJECT2.name = "Gaafar";
-console.log(OBJECT2); // {name: "Ahmed", sex: "male"}
+console.log(test());
 
 /////////////////////////
 
+// Regular function with no parameters
 
+let regularTest = function() {
+    return 22;
+}
+
+console.log(regularTest());
+
+/////////
+
+// Arrow function with no parameters: () or _
+
+let arrowTestOne = () => 222;
+
+console.log(arrowTestOne());
+
+let arrowTestOne2 = _ => 2222;
+
+console.log(arrowTestOne2());
+
+/////////////////////////
+
+// Regular function with one parameter
+
+let regularTest2 = function(param) {
+    return param * 2;
+}
+
+console.log(regularTest2(16));
+
+/////////
+
+// Arrow function with one parameter
+
+let arrowTestTwo = param => param * 2;
+
+console.log(arrowTestTwo(16));
+
+/////////////////////////
+
+// Regular function with multiple parameters
+
+let regularTest3 = function(param1, param2) {
+    return param1 * param2;
+}
+
+console.log(regularTest3(10, 30));
+
+/////////
+
+// Arrow function with multiple parameters
+
+let arrowTestThree = (param1, param2) => param1 * param2;
+
+console.log(arrowTestThree(10, 30));
+
+/////////////////////////
