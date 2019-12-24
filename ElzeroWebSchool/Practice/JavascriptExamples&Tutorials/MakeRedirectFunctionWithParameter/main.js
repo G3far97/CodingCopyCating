b0d2736c2493dfd0cab261c58d1b89074549e6aa
G3far97 <ahmed.fouadgaafar@student.guc.edu.eg>
2019-12-25 01:37:01 +0200

    MakeRedirectFunctionWithParameter

---
+++ ElzeroWebSchool/Practice/JavascriptExamples&Tutorials/MakeRedirectFunctionWithParameter/main.js
@@ -0,0 +1,17 @@
+/*
+    Lesson Name         : Make Redirect Function With Parameter
+    Difficulty          : Very Easy
+    What We Will Do     : Function, Window.Location and OnClick
+    Author              : ELzero Web School
+*/
+
+/////////////////////////
+
+function redirectMe(url) {
+    'use strict';
+    if(url !== '') {
+        window.location = url;
+    }
+}
+
+/////////////////////////
