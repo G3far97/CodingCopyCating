d00cc46547da54736e6a3eb4f65bb4168e790fb0
G3far97 <ahmed.fouadgaafar@student.guc.edu.eg>
2019-12-27 16:34:26 +0200

    Logger With Event Emitter

--- TraversyMedia/Node.jsCrashCourse/index.js
+++ TraversyMedia/Node.jsCrashCourse/index.js
@@ -1,5 +1,19 @@
-const Person = require('./person');
+// const Person = require('./person');
 
-const person1 = new Person('John Doe', 30);
+// const person1 = new Person('John Doe', 30);
 
-person1.greeting(); // 
+// person1.greeting(); // 
+
+/////////////////////////
+
+const Logger = require('./logger');
+
+const logger = new Logger();
+
+logger.on('message', data => console.log('Called Listener', data));
+
+logger.log('Hello World!');
+logger.log('Hi');
+logger.log('Hello');
+
+/////////////////////////
